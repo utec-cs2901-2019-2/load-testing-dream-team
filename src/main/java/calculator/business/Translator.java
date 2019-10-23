8c544649cb46b132eb1c70e28212c60ead65679b
juan4056 <juan.vargas@utec.edu.pe>
2019-10-23 12:52:37 -0500

    end

--- src/main/java/calculator/business/Translator.java
+++ src/main/java/calculator/business/Translator.java
@@ -1,8 +1,11 @@
 package calculator.business;
 
 import calculator.entities.Language;
-
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.Translate.TranslateOption;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 public interface Translator {
 
-    public String translate(Language from, Language to, String text);
+    public String translate(String from, String to, String text);
 }
